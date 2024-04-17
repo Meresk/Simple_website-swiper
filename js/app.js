38e5563df819c0baa4825c735f397901f2c9eab0
Meresk <Yakovx95@mail.ru>
2024-04-17 23:52:02 +0300

    add clock ver 0.1

--- js/app.js
+++ js/app.js
@@ -19,3 +19,26 @@ swiperText.on('slideChange', function() {
         ease: Power2.easeOut
     })
 })
+
+let hour = document.getElementById('hour')
+let min = document.getElementById('min')
+let sec = document.getElementById('sec')
+let hh,mm,ss,hRotation,mRotation,sRotation;
+
+function displayTime() {
+    let date = new Date();
+
+    hh = date.getHours();
+    mm = date.getMinutes();
+    ss = date.getSeconds();
+
+    hRotation = 30*hh + mm/2;
+    mRotation = 6*mm;
+    sRotation = 6*ss;
+
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+}
+
+setInterval(displayTime, 1000);
